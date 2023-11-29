320c331e923dd10f8bf8a3280c6ae0a18233162d
TUDjose <J.B.PintodeMouraLeitedaCunha@student.tudelft.nl>
2023-11-29 16:26:57 +0100

    added RBB description to model.py

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ABM-Grp5
+
+This is a repository for the ABM group project of group 5:
+- José Cunha - 5216087
+- xxx - xxx
+- xxx - xxx
+
+## Project description
+add in description of the ABM 
+
+add in link to ABBA RBB
--- model/main.py
+++ model/main.py
@@ -1,2 +1,8 @@
 # Just a test file for now
-print("Hello World!")
+"""
+RBB: Assimilative Social Influence, Nomina Opinions (e.g., my connection adapted/did not adapt)
+Develop an RBB that represents assimilative social influence with nominal opinions affecting flood adaptation behaviors
+in a neighborhood.
+https://jasss.soc.surrey.ac.uk/20/4/2.html
+"""
+
